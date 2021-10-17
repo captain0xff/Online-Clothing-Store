000a85411a7526d6fd8f85a2a9aecf1fe69a5a98
CAPTAIN1947 <87000456+CAPTAIN1947@users.noreply.github.com>
2021-10-17 19:59:49 +0530

    Add files via upload

---
+++ purchasing.py
@@ -0,0 +1,115 @@
+import PySimpleGUI as sg
+import mysql.connector as sqltor
+mycon = sqltor.connect(host = 'localhost', user = 'root', passwd = 'sayantan@sql', database = 'denim_destination_db')
+cursor = mycon.cursor()
+
+
+def purchaseMenu():
+    def cart(cartData1, price1):
+        sg.theme('DarkAmber')
+        heading1 = ['Product ID', 'Product Name', 'Brand', 'Size', 'Quantity', 'Price']
+        table=sg.Table(cartData1, headings=heading1, justification='left', key='-TABLE2-')
+        layout1 = [[sg.Text('YOUR CART')],
+                  [table],
+                  [sg.Text('Total Amount {}'.format(price1))],
+                  [sg.Button('Buy'), sg.Button('Go Back'),sg.Button('Clear Cart',key='CLR')]]
+        window1 = sg.Window('Your Cart', layout1, margins=(100, 50), finalize=True)
+        while True:
+            #print(cartData)
+            event1, values1 = window1.read()
+            print(event1,values1)
+            if event1 in (None, 'Go Back'):
+                break
+            if event1=='CLR':
+                cartData1=[]
+                table.update(cartData1)
+
+            if event1=='Buy':
+                confirm = sg.popup_yes_no('Are you sure you want to buy these products?')
+                if confirm == 'Yes':
+                    for i in cartData1:
+                        id1 = i[0]
+                        cursor.execute('SELECT * FROM PRODUCTS WHERE ID = %d' %id1)
+                        quantity = cursor.fetchall()
+                        print(id1, quantity)
+                        quantity = quantity[0][4]-i[4]
+                        if quantity>1:
+                            cursor.execute('UPDATE Products SET Quantity = {} WHERE ID = {}'.format(quantity, id1))
+                        else:
+                            cursor.execute('DELETE FROM Products WHERE ID = {}'.format(id1))
+                        mycon.commit()
+                    sg.popup_ok('Purchase Successful. Total Amount Spent = {}'.format(price1))
+                    break
+        window1.close()
+        return event
+
+    sg.theme('DarkAmber')
+
+    heading = ['Product ID', 'Product Name', 'Brand', 'Size', 'Quantity', 'Price']
+    cursor.execute('SELECT ID,Name,Brand,Size,Quantity,Selling_Price FROM PRODUCTS')
+    #Converted everything into list just to make things easier :)
+    data = list(cursor.fetchall())
+    for i in range(len(data)):
+        data[i] = list(data[i])
+    msg = sg.Text('',size=(20,0))
+    inp=sg.Input(key='-IN-')
+    layout = [[sg.Table(data, headings = heading, justification = 'centre', key = '-TABLE1-',enable_events=True)],
+              [sg.Text('Product ID:'), msg,
+               sg.Text(size=(20, 1), key='-OUTPUT-')],
+              [inp],
+              [sg.Button('Add to Cart'), sg.Button('Exit'), sg.Button('Go to Cart')]]
+
+    window = sg.Window('Products', layout, finalize = True)
+
+    price = 0
+    cartData = []
+    cartDict = {}
+    while True:
+        event, values = window.read()
+        if event in (None, 'Exit'):
+            break
+        if event=='-TABLE1-':
+            prod=str(values['-TABLE1-'][0]+1)
+            inp.update(prod)
+        if event =='Add to Cart':
+            # Declared the variable for my convenience and ease of understanding
+            prod_ID_selected = int(values['-IN-'])
+            cursor.execute('SELECT * FROM PRODUCTS WHERE ID = %d' %(prod_ID_selected))
+            proData = cursor.fetchall()
+            temp = (proData[0][0], proData[0][1], proData[0][2], proData[0][3], proData[0][6])
+            cartData.append(temp)
+            price+=proData[0][5]
+            i = temp
+            if i in cartDict:
+                cartDict[i][1] += i[4]
+                cartDict[i][0] += 1
+            else:
+                cartDict[i] = [1, i[4]]
+            print(cartDict)
+            # This part of the code is responsible for updating the table as we add items to cart
+            quantity = proData[0][4]
+            for i in range(len(data)):
+                prod_searched = list(data[i])
+                prod_searched.pop(4)
+                if prod_searched[0] == prod_ID_selected:
+                    data[i][4] = quantity-cartDict[tuple(prod_searched)][0]
+                    print(data)
+            msg.update('Added to Cart')
+            window['-IN-'].update('') #Clears the Input Window after we Add items to Cart
+            window['-TABLE1-'].update(data)
+
+        if event == 'Go to Cart' and len(cartDict)!=0:
+            cartDataFinal = []
+            for i in cartDict:
+                cartDataFinal.append(list(i)[:4]+cartDict[i])
+            action = cart(cartDataFinal, price)
+            if action=='Buy':
+                sg.popup_timed('Thank you for shopping with us')
+                window.close()
+                break
+        if event == 'Go to Cart' and len(cartDict)==0:
+            msg.update('Empty Cart...')
+        print(event, values)
+    mycon.commit()
+
+purchaseMenu()
