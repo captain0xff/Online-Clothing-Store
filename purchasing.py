2dfe367f30c2e68e76482c35d667365c37d37b46
Witty-31-06 <92244610+Witty-31-06@users.noreply.github.com>
2021-10-31 16:35:06 +0530

    CSV  File
    Now this py file saves the purchase info to csv file also

--- purchasing.py
+++ purchasing.py
@@ -1,10 +1,15 @@
 import PySimpleGUI as sg
 import mysql.connector as sqltor
-mycon = sqltor.connect(host = 'localhost', user = 'root', passwd = 'sayantan@sql', database = 'denim_destination_db')
+import settings as st
+from datetime import date
+import csv
+import os
+current = os.getcwd()
+mycon = sqltor.connect(host=st.host,user=st.user,passwd=st.password,database=st.database)
 cursor = mycon.cursor()
 
 
-def Main():
+def purchaseMenu(email):
     def cart(cartData1):
         nonlocal price
         sg.theme('DarkAmber')
@@ -15,15 +20,13 @@ def Main():
         layout1 = [[sg.Text('YOUR CART')],
                   [table],
                   [sg.Text('Total Amount = '), priceMsg],
-                  [buyButton, sg.Button('Go Back',key='GB'),sg.Button('Clear',key='CLR'),sg.Button('Remove',key='RM')]]
+                  [buyButton, sg.Button('Go Back'),sg.Button('Clear',key='CLR'),sg.Button('Remove',key='RM')]]
         window1 = sg.Window('Your Cart', layout1, margins=(100, 50), finalize=True)
         remove_from_cart=None
         while True:
             #print(cartData)
             event1, values1 = window1.read()
             if event1 in (None, 'Go Back'):
-                break
-            if event1=='GB':
                 break
             if event1=='CLR':
                 cartData1=[]
@@ -37,8 +40,10 @@ def Main():
                 print(remove_from_cart)
             print(remove_from_cart)
             if event1=='RM' and remove_from_cart!=None and len(cartData1)>remove_from_cart:
+                #print('hello')
+                #print(cartData1)
                 dat = cartData1[remove_from_cart]
-                price-=dat[5]
+                price-=float(dat[5])
                 var = 0
                 for i in cartDict:
                     if var==remove_from_cart:
@@ -57,21 +62,54 @@ def Main():
                         id1 = i[0]
                         cursor.execute('SELECT * FROM PRODUCTS WHERE ID = %d' %id1)
                         quantity = cursor.fetchall()
-                        print(id1, quantity)
+                        #print(id1, quantity)
                         quantity = quantity[0][4]-i[4]
                         if quantity>1:
                             cursor.execute('UPDATE Products SET Quantity = {} WHERE ID = {}'.format(quantity, id1))
                         else: 
                             cursor.execute('DELETE FROM Products WHERE ID = {}'.format(id1))
-                        mycon.commit()
+                    print(cartData1)
+                    
+                    """INVOICE NUMBER GENERATION"""
+                    today = date.today()
+                    pur_date = today.strftime("%Y-%m-%d") #Changing format to YYYY-MM-DD (i.e 2021-10-31)
+                    cursor.execute(f"SELECT COUNT(*) FROM PURCHASE WHERE Purchase_Date = '{pur_date}'")
+                    result = cursor.fetchone() #Basically returns number of entries in purchase table
+                    today = pur_date.replace('-','') #changing 2021-10-31 to 20211031
+                    c = str(result[0]+1).zfill(6)
+                    inv_num = today+'-'+c#format is %d%m%Y-N where N is cardinality of purchase
+                    #print(inv_num)
+                    query = f"""INSERT INTO PURCHASE VALUES('{inv_num}','{pur_date}',{price},'{email}')"""
+                    #print(query)
+                    cursor.execute(query)
+                    cursor.execute(f"SELECT Total_Price FROM CUSTOMERS WHERE Email_ID = '{email}'")
+                    pri = cursor.fetchone()[0]
+                    pri = int(pri)+price
+                    cursor.execute(f"UPDATE CUSTOMERS SET Total_Price = {pri} WHERE Email_ID = '{email}'")
                     sg.popup_ok('Purchase Successful. Total Amount Spent = {}'.format(price))
-        window1.close()
-        return event1
+                    window1.close()
+                    """----Beginning of CSV part----"""
+                    def export(cd,mail):
+                        fname = f"Customer data//{mail}.csv"
+                        with open(fname,'a',newline='') as fh:
+                            write = csv.writer(fh)
+                            for i in range(len(cd)):
+                                cd[i].append(pur_date)
+                                print(pur_date)
+                            cd = [['Prod_ID','Name','Brand','Size','Quantity','Cost','Pur Date']]+cd
+                            write.writerows(cd)
+                    try:
+                        os.mkdir("Customer data")
+                        export(cartData1,email)
+                    except FileExistsError:
+                        export(cartData1,email)
+                    """----End of CSV part----"""
+                    return event1
+
 
 
 
     sg.theme('DarkAmber')
-
     heading = ['Product ID', 'Product Name', 'Brand', 'Size', 'Quantity', 'Price']
     cursor.execute('SELECT ID,Name,Brand,Size,Quantity,Selling_Price FROM PRODUCTS')
     #Converted everything into list just to make things easier :)
@@ -107,7 +145,8 @@ def Main():
             #print(proData)
             temp = (proData[0][0], proData[0][1], proData[0][2], proData[0][3], proData[0][6])
             cartData.append(temp)
-            price+=temp[4]
+            price+=float(temp[4])
+            #print(price)
             if temp in cartDict:
                 cartDict[temp][1] += temp[4]
                 cartDict[temp][0] += 1
@@ -121,7 +160,7 @@ def Main():
                 prod_searched.pop(4)
                 if prod_searched[0] == prod_ID_selected:
                     data[i][4] = quantity-cartDict[tuple(prod_searched)][0]
-                    print(data)
+                    #print(data)
             msg.update('Added to Cart')
             window['-IN-'].update('') #Clears the Input Window after we Add items to Cart
             window['-TABLE1-'].update(data)
@@ -131,19 +170,20 @@ def Main():
 
         if event == 'Go to Cart' and len(cartDict)!=0:
             cartDataFinal = []
-            print(cartDict)
+            #print(cartDict)
             for i in cartDict:
                 cartDataFinal.append(list(i)[:4]+cartDict[i])
             action = cart(cartDataFinal)
             msg.update('')
-            print(action)
+            #print(action)
             if action=='Buy':
                 sg.popup_timed('Thank you for shopping with us')
                 window.close()
                 break
 
-        print(event, values)
+       # print(event, values)
     mycon.commit()
-
+#purchaseMenu('gauravchanda@gmail.com')
+mycon.commit()
 if __name__=='__main__':
-    Main()
+    purchaseMenu(ID)
