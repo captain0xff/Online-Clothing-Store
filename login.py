1251ff82f27e23b8fe4ebb5cde6713ecfa77d402
captain1947 <sayantan@gmail.com>
2021-11-21 21:39:35 +0530

    Timepass

--- login.py
+++ login.py
@@ -9,6 +9,8 @@ import sys
 import warnings
 import time
 import os
+
+#Timepass
 """Code for checking environment"""
 idle = True if 'idlelib.run' in sys.modules else False #Credit goes to stackexchange
 if idle == True:
